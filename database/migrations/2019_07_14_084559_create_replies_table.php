6874916ab42b023b78ec384dfb092e5e34d2c46a
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2019-07-18 10:29:01 +0700

    Relationships

--- database/migrations/2019_07_14_084559_create_replies_table.php
+++ database/migrations/2019_07_14_084559_create_replies_table.php
@@ -14,9 +14,10 @@ class CreateRepliesTable extends Migration
     public function up()
     {
         Schema::create('replies', function (Blueprint $table) {
+            Schema::dropIfExists('replies');
             $table->bigIncrements('id');
             $table->text('body');
-            $table->integer('question_id')->unsigned();
+            $table->bigInteger('question_id')->unsigned();
             $table->foreign('question_id')->references('id')
                 ->on('questions')
                 ->onDelete('cascade');
